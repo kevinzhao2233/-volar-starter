ae2b2f5c25a846ac29fae98d4532547a043a2e1c
KevinZhao <kevin_2233@qq.com>
2022-10-09 09:27:47 +0800

    Initialize monorepo

---
+++ packages/web/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { sayHello } from '@volar-starter/shared';
+
+sayHello();
+
+defineExpose({ sayHello });
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
